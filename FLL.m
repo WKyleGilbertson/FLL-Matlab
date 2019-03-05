c9dec8b53c831af0aaf00b14669c4de98c2ea6e6
W. Kyle Gilbertson <wkyleg@gmail.com>
2019-03-05 14:33:02 -0700

    Wasn't accumulating in the the accummulator

--- FLL.m
+++ FLL.m
@@ -10,19 +10,20 @@ ref = NCO(5, FSample);
 out = NCO(5, FSample);
 ref.SetFrequency(RefFreq);  % Fc = 9.548e6
 out.SetFrequency(OutFreq);
+I1 = I2 = Q1 = Q2 = 0;
 
 for idx = 1:30
  for n = 1:(FSample*PDItime)
   ref.clock();
   out.clock();
-  Q1 = ref.costable(ref.index) * out.costable(out.index);
-  I1 = ref.sintable(ref.index) * out.sintable(out.index);
+  Q1 = Q1 + ref.costable(ref.index) * out.costable(out.index);
+  I1 = I1 + ref.sintable(ref.index) * out.sintable(out.index);
  end % first ms (or PDI interval) of samples
  for n = 1:(FSample*PDItime)
   ref.clock();
   out.clock();
-  Q2 = ref.costable(ref.index) * out.costable(out.index);
-  I2 = ref.sintable(ref.index) * out.sintable(out.index);
+  Q2 = Q2 + ref.costable(ref.index) * out.costable(out.index);
+  I2 = I2 + ref.sintable(ref.index) * out.sintable(out.index);
  end % second ms (or PDI interval) of samples
 
 dot   = I1 * I2 + Q1 * Q2;
@@ -30,4 +31,5 @@ cross = I1 * Q2 - I2 * Q1;
 FreqError = atan2(cross, dot)/(2 * pi * PDItime);
 printf("%3d %9.3f\n", idx, FreqError);
 out.SetFrequency(out.Frequency + FreqError / 8); % How much adjustment?
+I1 = I2 = Q1 = Q2 = 0;
 end % of 30 sample for loop (60 ms)
