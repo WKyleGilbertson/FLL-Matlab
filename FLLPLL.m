bd6e4957d083e07e4cf14a386f5c9df47d94c1e8
W. Kyle Gilbertson <wkyleg@gmail.com>
2019-03-07 14:35:12 -0700

    Added some output descriptions

--- FLLPLL.m
+++ FLLPLL.m
@@ -1,4 +1,4 @@
-% Filename: FLL.m                                                    2018-03-01
+% Filename: FLLPLL.m                                                 2018-03-01
 %
 clear all;
 close all;
@@ -10,10 +10,10 @@ ref = NCO(5, FSample);
 out = NCO(5, FSample);
 ref.SetFrequency(RefFreq);  % Fc = 9.548e6
 out.SetFrequency(OutFreq);
-I1 = I2 = Q1 = Q2 = 0;
+I1 = I2 = Q1 = Q2 = 1;
 Phi = LastPhi = 0;
 Error = LastError = 0;
-Tau1 = 1 / 47.14; % Tau1 is wrong. Need to square Wn
+Tau1 = 1 / (47.14 * 47.14);
 Tau2 = 2 * 0.707 / 47.14;
 
 for idx = 1:500
@@ -23,7 +23,7 @@ for idx = 1:500
   Q1 = Q1 + ref.costable(ref.index) * out.costable(out.index);
   I1 = I1 + ref.sintable(ref.index) * out.sintable(out.index);
  end % first ms (or PDI interval) of samples
- Error = atan2(Q1,I1)/(2*pi);
+ Error = atan2(Q1,I1)/(2*pi); % Should be just atan(Q1/I1)
  Phi = LastPhi + Tau2/Tau1 * (Error - LastError) + Error * (PDItime/Tau1);
  LastPhi = Phi;
  LastError = Error;
@@ -36,7 +36,7 @@ for idx = 1:500
   Q2 = Q2 + ref.costable(ref.index) * out.costable(out.index);
   I2 = I2 + ref.sintable(ref.index) * out.sintable(out.index);
  end % second ms (or PDI interval) of samples
- Error = atan2(Q2,I2)/(2*pi);
+ Error = atan2(Q2,I2)/(2*pi); % Should be just atan(Q1/I1)
  Phi = LastPhi + Tau2/Tau1 * (Error - LastError) + Error * (PDItime/Tau1);
  LastPhi = Phi;
  LastError = Error;
@@ -47,9 +47,9 @@ dot   = I1 * I2 + Q1 * Q2;
 cross = I1 * Q2 - I2 * Q1;
 FreqError = atan2(cross, dot)/(2 * pi * PDItime);
 E(idx) = out.Frequency - RefFreq;
-printf("%3d %9.3f %9.3f %7.0f\n", idx, FreqError, E(idx), out.Frequency);
+printf("%3d FErr:%9.3f dF:%9.3f F:%7.0f\n", idx, FreqError, E(idx), out.Frequency);
 %plot(E);
 %out.SetFrequency(out.Frequency + FreqError * 1.0); % How much adjustment?
-I1 = I2 = Q1 = Q2 = 0;
+I1 = I2 = Q1 = Q2 = 1;
 end % of 30 sample for loop (60 ms)
 plot(E);
