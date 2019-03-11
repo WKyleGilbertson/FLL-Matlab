e16548432bd02adb530c388a369c6a14ab79f44b
W. Kyle Gilbertson <wkyleg@gmail.com>
2019-03-11 13:27:38 -0600

    Always use sine for reference. Cosine out signal forms Q; Sine out signal forms I

--- FLL.m
+++ FLL.m
@@ -3,7 +3,7 @@
 clear all;
 close all;
 RefFreq = 9548000;
-OutFreq = 9548150;
+OutFreq = 9548450;
 FSample = 38192000;
 PDItime = 0.001;  % PreDetection Interval // typically 1 ms
 ref = NCO(5, FSample);
@@ -16,14 +16,14 @@ for idx = 1:25
  for n = 1:(FSample*PDItime)
   ref.clock();
   out.clock();
-  I1 = I1 + ref.costable(ref.index) * out.costable(out.index);
-  Q1 = Q1 + ref.sintable(ref.index) * out.sintable(out.index);
+  I1 = I1 + ref.sintable(ref.index) * out.sintable(out.index);
+  Q1 = Q1 + ref.sintable(ref.index) * out.costable(out.index);
  end % first ms (or PDI interval) of samples
  for n = 1:(FSample*PDItime)
   ref.clock();
   out.clock();
-  I2 = I2 + ref.costable(ref.index) * out.costable(out.index);
-  Q2 = Q2 + ref.sintable(ref.index) * out.sintable(out.index);
+  I2 = I2 + ref.sintable(ref.index) * out.sintable(out.index);
+  Q2 = Q2 + ref.sintable(ref.index) * out.costable(out.index);
  end % second ms (or PDI interval) of samples
 
 dot   = I1 * I2 + Q1 * Q2;
@@ -32,6 +32,6 @@ FreqError = atan2(cross, dot)/(2 * pi * PDItime);
 NewFreq = out.Frequency + FreqError * 1.0;
 printf("%3d CalcErr:%9.3f ActErr:%11.3f Fnow:%11.3f Fnxt:%11.3f\n",...
         idx, FreqError, out.Frequency - ref.Frequency, out.Frequency, NewFreq);
-%out.SetFrequency(NewFreq);
+out.SetFrequency(NewFreq);
 I1 = I2 = Q1 = Q2 = 1;
 end % of 30 sample for loop (60 ms)
